d23848e1ae9f89d62231cb2eece8a0e20679be89
Alntjan <oliveiracatarino88@gmail.com>
2015-09-25 10:49:19 +0100

    Added the link param to the ideas and resources controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,14 @@
 class IdeasController < ApplicationController
   def create
     p = Project.find(params[:project_id])
-    p.ideas.create(params.require(:idea).permit(:name, :description, :user_id))
+    p.ideas.create(idea_params)
     redirect_to project_path(p)
   end
   def index
     @ideas = Idea.all
+  end
+  private
+  def idea_params
+    params.require(:idea).permit(:name, :description, :user_id, :link)
   end
 end
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,10 +1,15 @@
 class ResourcesController < ApplicationController
   def create
     p = Project.find(params[:project_id])
-    p.resources.create(params.require(:resource).permit(:name, :description, :user_id))
+    p.resources.create(resource_params)
     redirect_to project_path(p)
   end
   def index
     @resources = Resource.all
+  end
+
+  private
+  def resource_params
+    params.require(:resource).permit(:name, :description, :user_id, :link)
   end
 end
