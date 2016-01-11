c4842b4c5be8ee81244d00dbd6e567c8452f8a27
Alntjan <oliveiracatarino88@gmail.com>
2016-01-11 14:31:40 +0000

    Changed current_user Id to controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,19 @@
 class IdeasController < ApplicationController
   def create
-    p = Project.find(params[:project_id])
-    p.ideas.create(idea_params)
-    redirect_to project_path(p)
+    @project = Project.find(params[:project_id])
+    @idea = @project.ideas.build(idea_params)
+    @idea.user_id = current_user.id
+    if @idea.save
+      flash[:success] = "Ideia submetida com sucesso!"
+      redirect_to project_path(@project)
+    else
+      flash[:success] = "Erro!"
+    end
+    # p = Project.find(params[:project_id])
+    # p.ideas.create(idea_params)
+    # redirect_to project_path(p)
+
+
   end
   def index
     @ideas = Idea.all
